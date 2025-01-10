ece50dfded8be2c1f577e8cebe7d0ab20a7c9df0
thatssjames <thatsjames06@gmail.com>
2025-01-09 19:58:24 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   images: {
-	domains: ['tr.rbxcdn.com']
+    domains: ['tr.rbxcdn.com']
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false,
+      crypto: false
+    };
+    
+    return config;
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
